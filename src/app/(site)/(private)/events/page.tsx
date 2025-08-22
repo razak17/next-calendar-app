45c48ab7fd627dbece2fdac31089ac801c1717bc
Abdul Razak <moabdulrazak17@gmail.com>
2025-08-22 16:04:35 +0000

    feat: shadcn setup

---
+++ src/app/(site)/(private)/events/page.tsx
@@ -0,0 +1,8 @@
+export function EventsPage() {
+  return (
+    <main className="flex animate-fade-in flex-col gap-6 p-10">
+      <h1 className="font-black text-3xl">Events</h1>
+      <p className="font-extralight">Manage your events here.</p>
+    </main>
+  );
+}
